4ee4318d9496fb6b47e590175575cac0002f7599
lourencogabe <gabriels211@hotmail.com>
2025-04-24 23:00:29 -0300

    init: initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Date -->
+  <div>
+    {{ new Date().corretnt({'en-us',
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    })}}
+  </div>
+</template>
