6bd7e1bb278434993faee38fbbdf3980e29dfac0
lourencogabe <gabriels211@hotmail.com>
2025-04-25 17:37:57 -0300

    feat: add search input module

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <!-- Date -->
+  <!-- Data -->
+  <div class="text-center mb-6">
+    {{ formattedData }}
+  </div>
+  <!-- Barra de busca -->
   <div>
-    {{ new Date().corretnt({'en-us',
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    })}}
+    <searchInput />
   </div>
 </template>
+
+<script setup lang="ts">
+//Método para formatar a data
+const formattedData = new Date().toLocaleDateString('pt-br', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+})
+
+//Importa o componente de busca
+import searchInput from './components/search-input.vue'
+</script>
